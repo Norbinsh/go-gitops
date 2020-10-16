7927ae9d6f71bfa745b8f59fa7d5a42a975907f9
shay elmualem <shay.elmualem@checkmarx.com>
2020-10-16 16:41:59 +0300

    sample go app

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", goGitops)
+	http.ListenAndServe(":80", nil)
+}
+
+func goGitops(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "UserAgent: %s", r.UserAgent())
+}
